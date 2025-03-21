3f9c0ad2a10875a0e37778642a7d23761e821ff1
Arlia1 <ARLIAYOEL23@gmail.com>
2025-03-20 23:57:07 -0300

    Retomo proyecto actualizacion con todo por la mitad.

--- public/src/script.js
+++ public/src/script.js
@@ -1,10 +1,17 @@
 // Define the lists of video links
-console.log('Script loaded');
 const bailarinaVideos = [
     'https://drive.google.com/file/d/1-2p9obqHkQ2iiJqXIeUWj6e54uQBd9hs/preview',
+    'https://drive.google.com/file/d/1-ru0TV3MHB5VKxVd6yWqxYLt1iYJ_Npx/preview',
     'https://drive.google.com/file/d/12754PkHS5HeFayUNJb0yK_AaRjwBxj5g/preview',
     'https://drive.google.com/file/d/108MrAoYF48_q938lSHY6Pn1d2OdDZAgI/preview',
-    'https://drive.google.com/file/d/1-ru0TV3MHB5VKxVd6yWqxYLt1iYJ_Npx/preview'
+    
+];
+
+const bailarinaPreviews = [
+    'https://drive.google.com/file/d/19uHRTeINl6allMKsgNGfx6mELKP8XOOc/preview',
+    'https://drive.google.com/file/d/19voW_pVL3AjoimhDWt7hc-O3y0-RfhZm/preview',
+    'https://drive.google.com/file/d/19vsjqvAO6pIyQBa4Zl3Sa3N6qGPskXvd/preview',
+    'https://drive.google.com/file/d/19xYx8o7eejMlo5beMA4O5i35CQHWG_3O/preview',
 ];
 
 const profesoraVideos = [
@@ -23,6 +30,34 @@ const profesoraVideos = [
     'https://drive.google.com/file/d/11j8s3FlmyNxZFC8E5urBhOP4cdHxh9R9/preview',
     'https://drive.google.com/file/d/13MANLgxmlxIWzZ5nbCdxAOU90qOAwFCL/preview',
     'https://drive.google.com/file/d/13IBxYvztl5dj_90X59mVdGZKJbrWF0gl/preview' /* urbano */
+];
+
+const profesoraPreviews = [
+    // Add the preview URLs for profesora videos here
+    'https://drive.google.com/file/d/13rzsW8FSNUJgFznq6x-E9mDEQFkWiQT-/preview',
+    'https://drive.google.com/file/d/13Hmg7OzsliTIdoyZzBZNN8ejnZRaCx4I/preview',
+    'https://drive.google.com/file/d/13asjIlraXV8Yp5T4T9mCsBY3yx6ltop5/preview',
+    'https://drive.google.com/file/d/14QO89heFyFig2dIHNcyXG36W2Klqex86/preview',
+    'https://drive.google.com/file/d/13mRqtPv5dKNSZy4HvuC4iFRqywRygxvV/preview',
+    'https://drive.google.com/file/d/13mRqtPv5dKNSZy4HvuC4iFRqywRygxvV/preview',
+    'https://drive.google.com/file/d/11aoHUIvSQ72YhJlv8D-EDvw2YZjlQS71/preview',
+    'https://drive.google.com/file/d/11bg2UzXKYAgsrqaen6Vhd4SykQO67xpc/preview', /* heels */
+    'https://drive.google.com/file/d/142aRmQX9Kc3yTPTQITqw0GfX4w2flGNy/preview',
+    'https://drive.google.com/file/d/1421n0q4Icc6Ms4AHqRSme3aKJ6qic_0U/preview',
+    'https://drive.google.com/file/d/141NMza1fm1PSzoz8WIPcASp7yM-6Jx4C/preview',
+    'https://drive.google.com/file/d/141NMza1fm1PSzoz8WIPcASp7yM-6Jx4C/preview', /* kids/teen */
+    'https://drive.google.com/file/d/11j8s3FlmyNxZFC8E5urBhOP4cdHxh9R9/preview',
+    'https://drive.google.com/file/d/13MANLgxmlxIWzZ5nbCdxAOU90qOAwFCL/preview',
+    'https://drive.google.com/file/d/13IBxYvztl5dj_90X59mVdGZKJbrWF0gl/preview' /* urbano */
+];
+
+const formacionPreviews = [
+    // Add the preview URLs for formacion videos here
+    'https://drive.google.com/file/d/13IBxYvztl5dj_90X59mVdGZKJbrWF0gl/preview',
+    'https://drive.google.com/file/d/11nn2Vl6rVrrnFvA9UFXhQTwsjs_zTEe5/preview',
+    'https://drive.google.com/file/d/11xtMhEo9GLDLRzmqP-dzHXrUL8ydbj-A/preview',
+    'https://drive.google.com/file/d/11yKyV9dRXGBa87vPWTt477B6o9KE_jta/preview',
+    'https://drive.google.com/file/d/14IFJQzp9sn38LPT2CVP9a01cH1EqkwwT/preview'
 ];
 
 const formacionVideos = [
@@ -66,10 +101,21 @@ function changeVideo(listName, direction, iframeId) {
     iframeElement.src = currentList[currentIndex];
 }
 
-function handleSwipe(element, listName, iframeId) {
+// Function to start the video
+function startVideo(iframeId, previewId) {
+    let iframeElement = document.getElementById(iframeId);
+    let previewElement = document.getElementById(previewId);
+    previewElement.style.display = 'none';
+    iframeElement.style.display = 'block';
+}
+
+// Function to handle swipe events
+function handleSwipe(containerId, listName, iframeId) {
     let touchStartX = 0;
     let touchEndX = 0;
     const minSwipeDistance = 10; // Minimum distance for a swipe to be considered
+
+    let element = document.getElementById(containerId);
 
     element.addEventListener('touchstart', function(e) {
         console.log('Touch start detected');
@@ -79,46 +125,38 @@ function handleSwipe(element, listName, iframeId) {
     element.addEventListener('touchend', function(e) {
         console.log('Touch end detected');
         touchEndX = e.changedTouches[0].screenX;
-        handleSwipeDirection();
+        handleSwipeDirection();  // This will now access touchStartX and touchEndX
     });
+    
 
+    // Handle swipe direction based on the touch positions
     function handleSwipeDirection() {
         console.log('Handling swipe direction');
         let swipeDistance = touchEndX - touchStartX;
+
         if (Math.abs(swipeDistance) > minSwipeDistance) {
             if (swipeDistance < 0) {
                 console.log('Swipe left detected');
                 changeVideo(listName, 1, iframeId); // Swipe left
-            } else {
+            } else if (swipeDistance > 0) {
                 console.log('Swipe right detected');
                 changeVideo(listName, -1, iframeId); // Swipe right
-            }
-        }
+            } 
+        } else {  // If the swipe distance is too short, treat it as a click
+            console.log('Click detected');
+            let iframeElement = document.getElementById(iframeId);
+            iframeElement.click(); 
+        }   
     }
 }
 
+
 // Ensure the DOM is fully loaded before adding swipe event listeners
 document.addEventListener('DOMContentLoaded', function() {
-    // Create overlay divs for each iframe
-    createOverlay('bailarinaIframe', 'bailarina');
-    createOverlay('profesoraIframe', 'profesora');
-    createOverlay('formacionIframe', 'formacion');
+   handleSwipe('bailarinaContainer', 'bailarina', 'bailarinaIframe');
+   handleSwipe('profesoraContainer', 'profesora', 'profesoraIframe');
+   handleSwipe('formacionContainer', 'formacion', 'formacionIframe');
 });
-
-function createOverlay(iframeId, listName) {
-    let iframeElement = document.getElementById(iframeId);
-    let overlay = document.createElement('div');
-    overlay.style.position = 'absolute';
-    overlay.style.top = '0';
-    overlay.style.left = '0';
-    overlay.style.width = '100%';
-    overlay.style.height = '100%';
-    overlay.style.zIndex = '10';
-    overlay.style.backgroundColor = 'transparent';
-    iframeElement.parentElement.style.position = 'relative';
-    iframeElement.parentElement.appendChild(overlay);
-    handleSwipe(overlay, listName, iframeId);
-}
 
 window.onscroll = function() {
     let backToTopBtn = document.getElementById('backToTopBtn');
@@ -133,13 +171,18 @@ window.onscroll = function() {
         backToTopBtn.style.opacity = 0;
     }
 };
-// Add event listeners to buttons
-document.getElementById('backToTopBtn').addEventListener('click', function() {
-    window.scrollTo({ top: 0, behavior: 'smooth' });
+
+document.addEventListener('DOMContentLoaded', function() {
+    // Add event listeners to buttons
+
+    document.getElementById('backToTopBtn').addEventListener('click', function() {
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+    });
+
+    document.getElementById('bailarinaNextButton').addEventListener('click', () => changeVideo('bailarina', 1, 'bailarinaIframe'));
+    document.getElementById('bailarinaPrevButton').addEventListener('click', () => changeVideo('bailarina', -1, 'bailarinaIframe'));
+    document.getElementById('profesoraNextButton').addEventListener('click', () => changeVideo('profesora', 1, 'profesoraIframe'));
+    document.getElementById('profesoraPrevButton').addEventListener('click', () => changeVideo('profesora', -1, 'profesoraIframe'));
+    document.getElementById('formacionNextButton').addEventListener('click', () => changeVideo('formacion', 1, 'formacionIframe'));
+    document.getElementById('formacionPrevButton').addEventListener('click', () => changeVideo('formacion', -1, 'formacionIframe'));
 });
-document.getElementById('bailarinaNextButton').addEventListener('click', () => changeVideo('bailarina', 1, 'bailarinaIframe'));
-document.getElementById('bailarinaPrevButton').addEventListener('click', () => changeVideo('bailarina', -1, 'bailarinaIframe'));
-document.getElementById('profesoraNextButton').addEventListener('click', () => changeVideo('profesora', 1, 'profesoraIframe'));
-document.getElementById('profesoraPrevButton').addEventListener('click', () => changeVideo('profesora', -1, 'profesoraIframe'));
-document.getElementById('formacionNextButton').addEventListener('click', () => changeVideo('formacion', 1, 'formacionIframe'));
-document.getElementById('formacionPrevButton').addEventListener('click', () => changeVideo('formacion', -1, 'formacionIframe'));
